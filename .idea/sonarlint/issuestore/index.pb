742e186d8b2c63d17ba19d83cb19037a98ab532e
zacomoo <giacomo.puligheddu@hotmail.it>
2020-05-03 10:30:07 +0200

    Aggiunta implementazione vista Google Maps con GMapsFX

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -2,19 +2,24 @@
 :
 
 .gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
-J
-src/sample/Controller.java,a/9/a952ee24ccaf329067caf45d8e61416ce808b0ea
 N
 .idea/libraries/javafx_swt.xml,8/b/8b1fb17263788d9d0e3b511edaedee05b8afe4d6
 M
 .idea/artifacts/JavaFXApp.xml,4/d/4d10d7d793fd7ba352672035e41badf08b4fc2b5
 f
 6.idea/libraries/org_twitter4j_twitter4j_core_4_0_3.xml,6/f/6f1612313fb1f3c8fb4c177747e41e276ee53493
-F
-src/sample/sample.fxml,2/f/2f769fc3b0dd177c3365dff558843905f8609d7b
 @
 TweetTracker.iml,a/0/a0a0564ab721136cb2d5e35a75cedad2d55aee0e
-D
-src/sample/Main.java,a/d/ad8e8bd4ff8f02a914e3d9ca1bcc32dfc8c503a9
 S
 #.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
+D
+src/main/sample.fxml,e/b/ebd87776fb0593089b98bbc0d4aa7fc11d458da6
+B
+src/main/Main.java,f/e/fe03b4fd3afe162c6e01309b98a22588e093d274
+H
+src/main/Controller.java,7/6/76ee6fcea41cf68715ec13641a97e3ea456df8b1
+J
+src/main/TweetHandler.java,e/8/e8d9b0b16adc51e7e179955f00ccccb208b87be1
+:
+
+tweets.txt,2/7/27e3179c7ee927b00b1eea05a14ecfe2c8e76e78
--- src/main/Controller.java
+++ src/main/Controller.java
@@ -1,25 +1,65 @@
 package main;
 
+import com.lynden.gmapsfx.GoogleMapView;
+import com.lynden.gmapsfx.javascript.object.*;
+import com.lynden.gmapsfx.MapComponentInitializedListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable, MapComponentInitializedListener {
     @FXML
     private TextField searchBar;
     @FXML
     private ListView<String> listViewTweet;
-    private ObservableList<String> tweetList = FXCollections.observableArrayList();
-public void searchTweets(){
-    TweetHandler tweetHandler = new TweetHandler();
-    if (!searchBar.getText().isEmpty()) {
-        tweetList.addAll(tweetHandler.search(searchBar.getText()));
-        listViewTweet.setItems(tweetList);
-    }
-    else
-        System.out.println("Barra di ricerca vuota!");
-}
+    @FXML
+    private GoogleMapView mapView;
 
+    private GoogleMap map;
+    private double lat,lon;
+
+    private ObservableList<String> tweetList = FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        mapView.addMapInializedListener(this);
+    }
+
+    @Override
+    public void mapInitialized() {
+        LatLong latLong = new LatLong(41.9102415, 12.3959136);
+        MapOptions mapOptions = new MapOptions();
+        mapOptions.center(latLong)
+                .overviewMapControl(false)
+                .panControl(false)
+                .rotateControl(false)
+                .scaleControl(false)
+                .streetViewControl(false)
+                .zoomControl(false)
+                .zoom(6)
+                .mapType(MapTypeIdEnum.ROADMAP);
+
+        map = mapView.createMap(mapOptions);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLong);
+        Marker marker = new Marker(markerOptions);
+        map.addMarker(marker);
+        map.addMarker(new Marker(new MarkerOptions().position(new LatLong(40.27617,9.40193))));
+    }
+
+    public void searchTweets(){
+        TweetHandler tweetHandler = new TweetHandler();
+        if (!searchBar.getText().isEmpty()) {
+            tweetList.addAll(tweetHandler.search(searchBar.getText()));
+            listViewTweet.setItems(tweetList);
+        }
+        else
+            System.out.println("Barra di ricerca vuota!");
+    }
 }
